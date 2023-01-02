1e5b723c47b376102f04a4b8e24285c1fa6c85ac
Enrique <enriquec@mit.edu>
2023-01-02 18:28:14 -0500

    Flatten Chrome bookmarks and save them to storage

--- extension/public/app/background.js
+++ extension/public/app/background.js
@@ -1,20 +1,49 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
+/**
+ * Converts bookmarks
+ *
+ * @param {chrome.bookmarks.BookmarkTreeNode[]} bookmarks
+ */
+function flattenTree(bookmarks) {
+  const allBookmarks = [];
+  const allFolders = [];
+
+  for (let bookmark of bookmarks) {
+    if (!bookmark.children) {
+      // Leaf node, represents an actual bookmark
+      allBookmarks.push(bookmark);
+      continue;
+    }
+
+    const childrenIds = bookmark.children.map((child) => child.id);
+
+    // Node has children, represents a folder
+    allFolders.push({ ...bookmark, children: childrenIds });
+
+    // Recurse
+    const [flatChildren, flatFolders] = flattenTree(bookmark.children);
+    allBookmarks.push(...flatChildren);
+    allFolders.push(...flatFolders);
+  }
+
+  return [allBookmarks, allFolders];
+}
+
+/**
+ * Runs on first install
+ *
+ * Flattens the user's bookmarks and separates out the folders from the
+ * bookmarks themselves
+ */
+chrome.runtime.onInstalled.addListener(async () => {
+  console.log("Extension installed, loading bookmarks");
+
+  await chrome.storage.local.set({ firstUse: true });
+  console.debug("Set firstUse to TRUE");
+
+  const chromeBookmarks = await chrome.bookmarks.getTree();
+
+  // Create a deep copy of the Chrome bookmarks
+  const chromeBookmarksCopy = structuredClone(chromeBookmarks);
+  const [flatBookmarks, flatFolders] = flattenTree(chromeBookmarksCopy);
+  await chrome.storage.local.set({ bookmarks: flatBookmarks, folders: flatFolders });
 });
-
-chrome.action.onClicked.addListener(async (tab) => {
-  console.log(tab);
-  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-  // Next state will always be the opposite
-  const nextState = prevState === "ON" ? "OFF" : "ON";
-
-  // Set the action badge to the next state
-  await chrome.action.setBadgeText({
-    tabId: tab.id,
-    text: nextState,
-  });
-});
-
